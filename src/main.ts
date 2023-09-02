ca020d2347f18f0ca1a4ab55da1fa0165032353d
Javiera Quiñones <javiera.sandoval@devaid.cl>
2023-09-02 12:57:02 -0400

    Intento commit

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,6 @@ import { CartModule } from './cart/cart.module';
 import { ReportsModule } from './reports/reports.module';
 import { UsersModule } from './users/users.module';
 
-
-
 async function bootstrap() {
   console.log('Listen on port: 3000');
   const app = await NestFactory.create(AppModule);
