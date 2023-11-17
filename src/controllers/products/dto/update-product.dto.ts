e6e406dc97707aa06bdefb8924b7969ab60f7a8e
Javiera Quiñones <javiera.sandoval@devaid.cl>
2023-11-17 11:53:10 -0300

    Fix: fixes varios a los dtos de usuario y producto

--- src/controllers/products/dto/update-product.dto.ts
+++ src/controllers/products/dto/update-product.dto.ts
@@ -3,17 +3,4 @@ import { CreateProductDto } from './create-product.dto';
 import { IsDateString, IsInt, IsOptional, IsPositive, IsString, IsUUID, MaxLength, Min, MinLength, isDate, isDateString } from "@nestjs/class-validator";
 import { ApiProperty } from "@nestjs/swagger";
 
-export class UpdateProductDto extends PartialType(CreateProductDto) {
-
-    nombre?: string;
-    descripcion?: string;
-    precio?: number;
-    marca?: string;
-    avatar?: string;
-    tamanio?: string;
-    estado?: string;
-    material_cuadro?: string;
-    componentes?: string;
-    valoracion?: number;
-
-}
+export class UpdateProductDto extends PartialType(CreateProductDto) {}
